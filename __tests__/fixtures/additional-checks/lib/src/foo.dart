e35b62cea9763881bb842c72b2ecbfa0bfa5eda5
Sydney Jodon <sydney.jodon@workiva.com>
2025-08-08 13:46:57 -0700

    Add tests

---
+++ __tests__/fixtures/additional-checks/lib/src/foo.dart
@@ -0,0 +1,6 @@
+
+/// The following is intentionally commented out, it should not be included
+/// in the no_entrypoint_imports check
+// import 'package:additional_checks/additional_checks.dart';
+
+import 'package:additional_checks/src/dart/bar.dart';
